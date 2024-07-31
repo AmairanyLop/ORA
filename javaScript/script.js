0ae797ed5bac589b8ee6ac18893f00af66b07441
Amairany <loperenaamairany2@gmail.com>
2024-07-31 17:09:47 -0600

    modified:   htmq/index.html 	modified:   javaScript/script.js 	modified:   styles/style.css

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,11 +1,11 @@
 
-const textArea = document.querySelector(".area1");
-const mensaje = document.querySelector(".area2");
+const textArea = document.querySelector(".text-area");
+const mensaje = document.querySelector(".mensaje");
 
 function encriptar(stringEncriptado) {
     let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
     stringEncriptado = stringEncriptado.toLowerCase()
-   
+
     for (let i = 0; i < matrizCodigo.length; i++) {
         if (stringEncriptado.includes(matrizCodigo[i][0])) {
             stringEncriptado = stringEncriptado.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
@@ -18,27 +18,32 @@ function encriptar(stringEncriptado) {
 function btnEncriptar() {
     const textoEncrip = encriptar(textArea.value);
     mensaje.value = textoEncrip;
-    textArea.value = " ";
+  
 }
 
 function desencriptar(stringDesencriptado) {
     let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
-    stringDesencriptado =  stringDesencriptado.toLowerCase()
-   
-    for (let i = 0; i < matrizCodigo.length; i++) {
-        if ( stringDesencriptado.includes(matrizCodigo[i][1])) {
-            stringDesencriptado =  stringDesencriptado.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
+    stringDesencriptado = stringDesencriptado.toLowerCase()
 
-        } 
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringDesencriptado.includes(matrizCodigo[i][1])) {
+            stringDesencriptado = stringDesencriptado.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
+
+        }
     }
-    return  stringDesencriptado
+    return stringDesencriptado
 }
 
 
 function btnDesencriptar() {
     const textoDesencrip = desencriptar(textArea.value);
     mensaje.value = textoDesencrip;
-    mensaje.value = " ";
+  
+}
+
+function btnlimpiar(){
+   textArea.value = "  "; 
+
 }
 
 document.querySelector(".btn-encriptar").addEventListener("click", btnEncriptar);
